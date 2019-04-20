cb3d9507501c9c891df34504c06e56d472c50ff4
Chernenko Viktoriya <4743866@mail.ru>
2019-04-20 21:48:51 +0300

    Подключение стилей #5

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import css from './styles/main.styl';
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
+requireAll(require.context('./', true, /\.styl$/));
 
